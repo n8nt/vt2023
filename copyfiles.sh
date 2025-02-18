76bd119292869f148883878d2af0938f80561534
Bob Tournoux <n8nt@atco.tv>
2025-02-17 21:58:41 -0500

    Create copyfiles.sh
    First attempt at script to copy new files to the raspberry pi from github

---
+++ copyfiles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd /usr/local/apps/deployer/vt2023
+tar -xzvf vtsdr.tar.gz
+echo "Backing up files to be updated"
+mkdir backup
+cp /usr/local/apps/versatune/versatune.jar backup/.
+cp /usr/local/apps/versatune/data/CombiTunerExpress backup/.
+cp /home/pi/.profile backup/.
+cp /home/pi/.bashrc backup/.
+cp /usr/local/apps/versatune/scripts/restartCombiTunerExpress.sh backup/.
+cp /usr/local/apps/versatune/scripts/htlp.txt backup/.
+echo "updating files"
+cp versatune/versatune-2.2.0.3.jar /usr/local/apps/versatune/versatune.jar
+cp versatune/data/CombiTunerExpress /usr/local/apps/versatune/data/CombiTunerExpress
+chmod 755 /usr/local/apps/versatuen/data/CombitunerExpress
+cp versatune/scripts/help.txt /usr/local/apps/versatune/scripts/help.txt
+cp versatune/scripts/restartCombiTunerExpress.sh /usr/local/apps/versatune/scripts/restartCombiTunerExpress.sh
+cp versatune/homepi/dotProfile /home/pi/.profile
+cp versatune/homepi/dotBashrc /home/pi/.bashrc
+cp updatevt.sh /home/pi/.
+echo "Finished"
