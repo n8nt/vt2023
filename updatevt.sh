7b92ecae7cc2ab583c47bd7607b266648a93f988
Bob Tournoux <n8nt@atco.tv>
2025-02-17 21:37:11 -0500

    Create updatevt.sh
    Add this script to the download

---
+++ updatevt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd /usr/local/apps/
+
+DIR="/usr/local/apps/deployer"
+if [ -d "$DIR" ]; then
+  # go ahead and do the git retrieve
+  echo "Directory '$DIR' exists."
+else
+  echo "Directory '$DIR' does not exist. Creating it now."
+  mkdir /usr/local/apps/deployer
+fi
+
+DIR2="/usr/local/apps/deployer/vt2023"
+if [ -d "$DIR2" ]; then
+  # need to delete the directory to be able to clone a new on.
+  echo "Deleting '$DIR2'"
+  rm  -rf /usr/local/apps/deployer/vt2023
+fi
+
+cd /usr/local/apps/deployer
+rm -rf versatune
+git clone https://github.com/n8nt/vt2023.git
+cd vt2023
